0eee45792f7e60dc6f14c1c9b05fe3d0ec034642
Kan <109953535+KanNattawat@users.noreply.github.com>
2023-02-24 14:16:21 +0700

    Add files via upload

---
+++ Lab06/Programmer.java
@@ -0,0 +1,21 @@
+public class Programmer extends Employee {
+    private int happiness;
+    public void coding(String str) {
+        if (getEnergy() >= 30) {
+            System.out.println("Your code is " + str);
+        } else {
+            System.out.println("Error Error Error");
+        }
+        setEnergy(getEnergy() - 30);
+        happiness -= 30;
+    }
+    public void coding(char str) {
+       this.coding(String.valueOf(str));
+    }
+    public int getHappiness() {
+        return happiness;
+    }
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
+    }
+}
